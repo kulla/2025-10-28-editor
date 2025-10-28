9729a692c87963af4835151f55576fff0e2fc272
Stephan Kulla <git.mail@kulla.me>
2025-10-28 13:53:46 +0100

    feat: Add src/nested-node.ts

---
+++ src/nested-node.ts
@@ -0,0 +1,30 @@
+import type {
+  ArraySchema,
+  BooleanSchema,
+  ObjectSchema,
+  Schema,
+  StringSchema,
+  UnionSchema,
+  WrapperSchema,
+} from './schema'
+
+export interface NestedNode<S extends Schema> {
+  schema: S
+  value: JSONValue<S>
+}
+
+type JSONValue<S extends Schema> = [S] extends [unknown] ? _JSONValue<S> : never
+
+type _JSONValue<S extends Schema> = S extends StringSchema
+  ? string
+  : S extends BooleanSchema
+    ? boolean
+    : S extends ArraySchema<infer I>
+      ? _JSONValue<I>[]
+      : S extends ObjectSchema<infer F>
+        ? { [K in keyof F]: _JSONValue<F[K]> }
+        : S extends WrapperSchema<infer C>
+          ? _JSONValue<C>
+          : S extends UnionSchema<infer O>
+            ? _JSONValue<O[number]>
+            : never
