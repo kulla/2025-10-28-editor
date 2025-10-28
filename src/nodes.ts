4fa590449c11a2f329e7a1b3d6ae2c8d784694c7
Stephan Kulla <git.mail@kulla.me>
2025-10-28 13:22:02 +0100

    feat: Add src/nodes.ts

---
+++ src/nodes.ts
@@ -0,0 +1,21 @@
+import * as S from './schema'
+
+const Text = S.string()
+
+const Paragraph = S.wrapper(Text)
+
+const Content = S.array(Paragraph)
+
+const MultipleChoiceExercide = S.object({
+  exercise: Content,
+  answers: S.array(
+    S.object({
+      text: Text,
+      isCorrect: S.boolean(),
+    }),
+  ),
+})
+
+const Document = S.array(S.union(Content, MultipleChoiceExercide))
+
+const Root = S.wrapper(Document)
